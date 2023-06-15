10d7f982c23884c12b259522d977b4939baa4d0e
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-15 18:26:42 -0500

    Header setup for export

--- src/container/index.js
+++ src/container/index.js
@@ -3,11 +3,13 @@ import Chef from './Chef/Chef';
 import FindUs from './FindUs/FindUs';
 import Footer from './Footer/Footer';
 import Gallery from './Gallery/Gallery';
+import Header from './Header/Header';
 
 export {
     AboutUs,
     Chef,
     FindUs,
     Footer,
-    Gallery
+    Gallery,
+    Header
 }
