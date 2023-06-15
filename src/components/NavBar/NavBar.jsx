b6bef728a70f76642ddd7ec6a01fe4426205245d
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-15 18:43:33 -0500

    icons imported

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+
+import './NavBar.css';
 
 const NavBar = () => {
     return (
