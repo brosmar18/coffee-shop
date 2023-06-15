7e6333f9de85fc146160194a068c757bdf5bd0a5
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-15 18:52:28 -0500

    Links added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,6 +10,13 @@ const NavBar = () => {
             <div className='nav__logo'>
                 <img src='https://picsum.photos/640/360' alt='logo' />
             </div>
+            <ul className='nav__links'>
+                <li className='nav__link'><a href='/'>Home</a></li>
+                <li className='nav__link'><a href='/'>About</a></li>
+                <li className='nav__link'><a href='/'>Menu</a></li>
+                <li className='nav__link'><a href='/'>Awards</a></li>
+                <li className='nav__link'><a href='/'>Contact</a></li>
+            </ul>
         </div>
     )
 }
