d7f1629835d4e2496df4745d8b3d81118dbad247
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-15 18:25:54 -0500

    Footer setup for export

--- src/container/index.js
+++ src/container/index.js
@@ -1,9 +1,11 @@
 import AboutUs from './AboutUs/AboutUs';
 import Chef from './Chef/Chef';
 import FindUs from './FindUs/FindUs';
+import Footer from './Footer/Footer';
 
 export {
     AboutUs,
     Chef,
-    FindUs
+    FindUs,
+    Footer
 }
