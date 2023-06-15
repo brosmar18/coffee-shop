c4f3596cb2686733c1b893442448c12a870cab7c
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-15 18:26:16 -0500

    Gallery setup for export

--- src/container/index.js
+++ src/container/index.js
@@ -2,10 +2,12 @@ import AboutUs from './AboutUs/AboutUs';
 import Chef from './Chef/Chef';
 import FindUs from './FindUs/FindUs';
 import Footer from './Footer/Footer';
+import Gallery from './Gallery/Gallery';
 
 export {
     AboutUs,
     Chef,
     FindUs,
-    Footer
+    Footer,
+    Gallery
 }
