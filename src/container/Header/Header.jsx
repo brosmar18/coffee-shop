2b4d3c8392d642d6c25e4bb12451f05fa2ff3ceb
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-06-20 14:57:34 -0500

    menu button added

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,6 +9,8 @@ const Header = () => {
             <div className='app__wrapper_info'>
                 <SubHeading />
                 <h1 className='app__header-h1'>The Key To Fine Dining</h1>
+                <p className='p__opensans' style={{ margin: '2rem 0'}}>Call to action or attention getter</p>
+                <button type='button' className='custom__button'>Explore Menu</button>
             </div>
         </div>
     )
