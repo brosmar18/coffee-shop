f243a8bb1cb831bbcf4866b97d93025a1e9382f6
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-06-20 14:50:08 -0500

    basic func component setup

---
+++ src/components/SubHeading/SubHeading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {images} from '../../constants';
+
+const SubHeading = () => {
+    <div style={{ marginBottom: '1rem' }}>
+        <p className='p__cormorant'>Title</p>
+    </div>
+}
+
+export default SubHeading;
