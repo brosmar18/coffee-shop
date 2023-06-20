b7a137681f0e59feeb4d291c46fd26d8e30c6ecd
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-06-20 14:54:54 -0500

    SubHeading added to Header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import {SubHeading} from '../../components';
+import {images} from '../../constants';
+
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className='app__header app__wrapper section_padding' id='home'>
+            <div className='app__wrapper_info'>
+                <SubHeading />
+            </div>
+        </div>
     )
 }
 
