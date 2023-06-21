fb2f462ad2551865d31958c207c6ef6be1e67142
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-06-21 00:17:48 -0500

    SubHeading added

--- src/container/Menu/Menu.jsx
+++ src/container/Menu/Menu.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import {data, images} from '../../constants';
+import './Menu.css';
 
 const Menu = () => {
     return (
-        <div>Menu</div>
+        <div>
+            <div>
+                <SubHeading title="Menu that fits your palatte" />
+            </div>
+        </div>
     )
 }
 
