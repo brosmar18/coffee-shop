55b9a9af5eb9e6904cf806e80f5b476c82c55ce2
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-06-20 20:07:50 -0500

    description added

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -7,6 +7,8 @@ const AboutUs = () => {
         <div className='app__aboutus-content flex__center'>
             <div className='app__aboutus-content_about'>
                 <h1 className='headtext__cormorant'>About Us</h1>
+                <img src={images.spoon} alt='about_spoon' className='spoon__img' />
+                <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusantium consequatur incidunt quos impedit sit, nobis non laudantium voluptatum maxime adipisci earum vero! Sit quos et recusandae aperiam iste repellendus.</p>
             </div>
         </div>
     )
