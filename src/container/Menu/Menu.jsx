06407f1f9e9b19c32d8f7a93b49828a24a47df30
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-06-21 00:19:31 -0500

    class names and title added

--- src/container/Menu/Menu.jsx
+++ src/container/Menu/Menu.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import { SubHeading } from '../../components';
-import {data, images} from '../../constants';
+import { data, images } from '../../constants';
 import './Menu.css';
 
 const Menu = () => {
     return (
-        <div>
-            <div>
+        <div className="app__specialMenu flex__center section__padding" id="menu">
+            <div className="app__specialMenu-title">
                 <SubHeading title="Menu that fits your palatte" />
+                <h1 className="headtext__cormorant">Today&apos;s Special</h1>
             </div>
         </div>
     )
