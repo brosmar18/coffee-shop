386040d07cd6f1ca5ea19e98d4647d19c8db0d69
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-06-20 20:06:34 -0500

    containers and title added

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -4,7 +4,11 @@ import './AboutUs.css';
 
 const AboutUs = () => {
     return (
-        <div>About Us</div>
+        <div className='app__aboutus-content flex__center'>
+            <div className='app__aboutus-content_about'>
+                <h1 className='headtext__cormorant'>About Us</h1>
+            </div>
+        </div>
     )
 }
 
