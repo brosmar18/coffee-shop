5cc82d5a7dcb7f81ab00c07adf3b83165e1db7c2
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-06-20 19:22:49 -0500

    app__header-h1 styles added

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
+import './Header.css';
 
 const Header = () => {
     return (
