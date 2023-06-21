b98d34b50f40a2e7918292cc0e58c05fc86a1e4f
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-06-20 20:51:59 -0500

    style and statements updated

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -13,7 +13,7 @@ const AboutUs = () => (
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <p className="p__opensans">Cafe del Sol, your neighborhood spot for artisan coffee and inspired Mexican flavors. We offer a blend of the warmth of the sun, the comfort of community, and a culinary journey through every bite and sip.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
 
@@ -24,7 +24,7 @@ const AboutUs = () => (
       <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
+        <p className="p__opensans">Founded in 2023, Cafe del Sol grew from a simple love for coffee and vibrant Mexican cuisine. Over the years, we've nurtured a space that fosters connection, welcomes diversity, and celebrates the sunniest aspects of life, one flavorful meal at a time.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
     </div>
