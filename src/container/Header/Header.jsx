ab36c668d0369f8591a370da186443f0c498f81e
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-06-20 19:31:48 -0500

    title and call to action updated

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -10,9 +10,9 @@ const Header = () => {
             <div className='app__header app__wrapper section_padding' id='home'>
                 <div className='app__wrapper_info'>
                     <SubHeading />
-                    <h1 className='app__header-h1'>The Key To Fine Dining</h1>
-                    <p className='p__opensans' style={{ margin: '2rem 0' }}>Call to action or attention getter</p>
-                    <button type='button' className='custom__button'>Explore Menu</button>
+                    <h1 className='app__header-h1'>Where the Sun Shines on Every Cup</h1>
+                    <p className='p__opensans' style={{ margin: '2rem 0' }}>Explore our menu and let your tastebuds rejoice at Cafe del Sol</p>
+                    <button type='button' className='custom__button'>Menu</button>
                 </div>
             </div>
 
