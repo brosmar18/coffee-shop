454464efef38f91e169f8b41cd51b2987cd3f0b6
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-06-22 11:52:03 -0500

    classNames and export added

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import './MenuItem.css';
 
 const MenuItem = () => {
-    <div>
-        <div>
-            <div>
-                <p>Title</p>
+    <div className="app__menuitem">
+        <div className="app__menuitem-head">
+            <div className="app__menuitem-name">
+                <p className="p__cormorant" style={{ color: '#DCCA87'}}>Title</p>
             </div>
         </div>
-        <div>
-            <div>
-                <p>Price</p>
+        <div className="app__menuitem-dash">
+            <div className="app__menuitem-price">
+                <p className="p__cormorant">Price</p>
             </div>
         </div>
-        <div>
-            <p>Tags</p>
+        <div className="app__menuitem-sub">
+            <p className="p__opensans">Tags</p>
         </div>
     </div>
 }
+
+export default MenuItem;
