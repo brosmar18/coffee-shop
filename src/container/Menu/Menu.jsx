3ee1bcd04966005ebc1787512aecd285c2701f76
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-06-22 11:26:12 -0500

    wine and beer section added

--- src/container/Menu/Menu.jsx
+++ src/container/Menu/Menu.jsx
@@ -11,6 +11,16 @@ const Menu = () => {
                 <SubHeading title="Menu that fits your palatte" />
                 <h1 className="headtext__cormorant">Today&apos;s Special</h1>
             </div>
+            <div className="app__specialMenu-menu">
+                <div className="app__specialMenu-menu_wine">
+                    <p className="app__specialMenu-menu_heading">Wine & Beer</p>
+                    <div className="app__specialMenu-menu_items">
+                        {data.wines.map((wine, index) => (
+                            <p>{wine.title}</p>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
