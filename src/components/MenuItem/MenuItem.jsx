905ce700c418a2f734962bf19399db1a3ea27509
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-06-22 11:49:06 -0500

    title added

---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MenuItem.css';
+
+const MenuItem = () => {
+    <div>
+        <div>
+            <div>
+                <p>Title</p>
+            </div>
+        </div>
+    </div>
+}
